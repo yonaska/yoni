10dd27fe4c4ef518f35dd5f526da8da2b011e668
yonaska <yonas.kahsay@georgebrown.ca>
2024-11-24 17:54:08 -0500

    Add files via upload

---
+++ lab12.sql.sql
@@ -0,0 +1,20 @@
+# yoni
+~ 
+-! (expalation)
+-@ (at)
+- # (hash)
+- $ (Dollars)
+- % (percent)
+- ^ (logical and= wedge)
+- & (ampersand)
+- * (Asterisk)
+  (   ( open becket)
+    ) ( close brackt)
+    _ (dash)
+  + (puls)
+  / (slash)
+    * (multiplication)
+- (minus)
+- -/+ volume
+- "links"
+- [eritrea](https://www.voanews.com/a/tensions-deepen-with-addis-ababa-falsely-accusing-cairo-of-aiding-eritrea-to-secede-decades-ago/7789115.html)
